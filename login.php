0210fe565a19fc707f4683df43b52c2d101090cd
shakir1443 <shakir1443@gmail.com>
2017-11-14 01:05:32 +0500

    login verification

---
+++ login.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html >
+<head>
+  <meta charset="UTF-8">
+  <title>Welcome to Automated Mushroom Cultivation System</title>
+  
+  
+  
+      <link rel="stylesheet" href="assets/css/loginstyle.css">
+
+  
+</head>
+
+<body bgcolor="#1ce0cf">
+   <div class="wrapper">
+	<div class="container">
+	    <h2>Welcome To</h2>
+		<h2>Automated Mushroom Cultivation System</h2>
+		
+		<form class="form" action = "log.php" method = "post">
+			<input type="text" name = "username" class="form-control" placeholder="Username">
+			<input type="password" name = "password" class="form-control" placeholder="Password">
+			<button type="submit" name="formsubmitbutton" id="lformsubmitbutton">Login</button>
+		</form>
+	</div>
+	
+	<ul class="bg-bubbles">
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+	</ul>
+</div>
+  <script src='assets/js/jquery.min.js'></script>
+
+    <script  src="assets/js/login.js"></script>
+</body>
+</html>
